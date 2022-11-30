1d7560c12cc27f620443d04948f87708097f72a9
zkhr <git@ariblumenthal.com>
2022-11-30 18:26:08 +0000

    add panel metadata, which includes a new url suffix.

--- static/js/me.js
+++ static/js/me.js
@@ -2,6 +2,9 @@ const panelMatrixEl = document.getElementById("panel-matrix");
 
 const DEFAULT_COORDINATES = { x: 0, y: 0 };
 
+/** A map from coordinates to metadata about a panel. */
+const panelMetadataMap = loadPanels();
+
 /** Tracks the coordinates of the currently rendered panel. */
 let currentCoord = getStartingCoordinates();
 
@@ -11,6 +14,23 @@ panelMatrixEl.style.display = "";
 initMatrixLinks();
 initKeyboardNavigation();
 initTouchscreenNavigation();
+
+function loadPanels() {
+  const panelEls = document.getElementsByClassName("panel");
+  const results = new Map();
+  for (const panelEl of panelEls) {
+    const x = parseInt(panelEl.dataset.x);
+    const y = parseInt(panelEl.dataset.y);
+    results.set(getPanelMetadataMapKey(x, y), {
+      urlSuffix: panelEl.dataset.urlSuffix,
+    });
+  }
+  return results;
+}
+
+function getPanelMetadataMapKey(x, y) {
+  return `(${x},${y})`;
+}
 
 function getStartingCoordinates() {
   const results = /\!\/([-\d]+)\/([-\d]+)/.exec(window.location.href);
@@ -126,5 +146,16 @@ function goToCoordinate(x, y) {
   panelMatrixEl.style.left = `${-100 * x}vw`;
   panelMatrixEl.style.top = `${-100 * y}vh`;
   currentCoord = { x, y };
-  window.history.replaceState(null, "", `/!/${x}/${y}`);
+  const path = getCoordinatePath(x, y);
+  window.history.replaceState(null, "", path);
+}
+
+/** Returns the path to use in the URL for the provided coordinates. */
+function getCoordinatePath(x, y) {
+  const metadata = panelMetadataMap.get(getPanelMetadataMapKey(x, y));
+  let path = `/!/${x}/${y}`;
+  if (metadata && metadata.urlSuffix) {
+    path += `/${metadata.urlSuffix}`;
+  }
+  return path;
 }
