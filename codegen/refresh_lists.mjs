eac27da5a347130beca3ed52cee958cbbda97f4f
zkhr <git@ariblumenthal.com>
2023-03-05 02:33:51 +0000

    Remove dialogues and add a couple books I finished recently.

--- codegen/refresh_lists.mjs
+++ codegen/refresh_lists.mjs
@@ -1,6 +1,6 @@
 /**
- * Generates the blog.html and dialogue.html panels based on other panels in the
- * panels/ directory.
+ * Generates the blog.html panel based on other panels in the panels/
+ * directory.
  *
  * Usage: `node ./codegen/refresh_blog.mjs`
  */
@@ -9,20 +9,16 @@ import { readdir, readFile, writeFile } from "node:fs/promises";
 import { JSDOM } from "jsdom";
 
 const blogFilename = "./panels/blog.html";
-const dialogueFilename = "./panels/dialogues.html";
 
 const blogEntries = [];
-const dialogueEntries = [];
 
 const filenames = await readdir("panels/");
 for (const filename of filenames.reverse()) {
   const isBlog = filename.indexOf("blog-") >= 0;
-  const isDialogue = filename.indexOf("dialogue-") >= 0;
-  if (!isBlog && !isDialogue) {
+  if (!isBlog) {
     continue;
   }
 
-  const entries = isBlog ? blogEntries : dialogueEntries;
   const htmlContent = await readFile(`panels/${filename}`, {
     encoding: "utf8",
   });
@@ -33,7 +29,7 @@ for (const filename of filenames.reverse()) {
   const title = panel.querySelector(".title").textContent;
   const xCoord = panel.dataset.x;
   const yCoord = panel.dataset.y;
-  entries.push(
+  blogEntries.push(
     `<div class="blog-entry">
       <div class="blog-entry-date">${date} -</div>
       <div class="link" data-x="${xCoord}" data-y="${yCoord}">${title}</div>
@@ -54,27 +50,6 @@ await writeFile(
   <div class="panel-content">
     <div class="title">Journal</div>
     <div class="blog-entries">${blogEntries.join("")}
-    </div>
-    <div class="nav-section">
-      <span class="link nav-link" data-x="0" data-y="0">Home</span>
-    </div>
-  </div>
-</div>`
-);
-
-console.log(`Updating ${dialogueFilename}`);
-await writeFile(
-  dialogueFilename,
-  `<div
-  class="panel dialogue-theme"
-  data-x="1"
-  data-y="1"
-  data-type="dialogue"
-  data-url-suffix="dialogues"
->
-  <div class="panel-content">
-    <div class="title">Dialogues</div>
-    <div class="blog-entries">${dialogueEntries.join("")}
     </div>
     <div class="nav-section">
       <span class="link nav-link" data-x="0" data-y="0">Home</span>
